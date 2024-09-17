f9d6e3fb2240afdc2e7ed45ae6cf1589bc6bcbe3
Johannes Johansson <mouwser1998@outlook.com>
2024-09-17 13:10:47 +0200

    frontend desing etc

--- Grupp4forum.Dev.App/wwwroot/clientapp/src/main.ts
+++ Grupp4forum.Dev.App/wwwroot/clientapp/src/main.ts
@@ -5,6 +5,8 @@ import router from './router'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
+import './assets/site.css';
+
 
 
 createApp(App).use(router).mount('#app')
