a48ef808dfd060322dd2b97c46b34fa2f385c931
Johannes Johansson <143495185+mouwser@users.noreply.github.com>
2024-09-18 10:38:49 +0200

    Update PasswordService.cs

--- Grupp4forum.Dev.Infrastructure/Services/PasswordService.cs
+++ Grupp4forum.Dev.Infrastructure/Services/PasswordService.cs
@@ -1,25 +1,16 @@
-﻿using Isopoh.Cryptography.Argon2;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Isopoh.Cryptography.Argon2;
 
-namespace Grupp4forum.Dev.Infrastructure.Services
+public class PasswordService
 {
-    public class PasswordService
+    // Hash(a) lösenord med Argon2
+    public string HashPassword(string password)
     {
-        // Hash(a) lösenord med Argon2
-        public string HashPassword(string password)
-        {
-            return Argon2.Hash(password);
-        }
+        return Argon2.Hash(password);
+    }
 
-        // Verifiera att ett lösenord matchar en given hash
-        public bool VerifyPassword(string password, string hashedPassword)
-        {
-            return Argon2.Verify(hashedPassword, password);
-        }
+    // Verifiera att ett lösenord matchar en given hash
+    public bool VerifyPassword(string password, string hashedPassword)
+    {
+        return Argon2.Verify(hashedPassword, password);
     }
 }
