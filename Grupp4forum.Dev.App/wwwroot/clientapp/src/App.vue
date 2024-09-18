cf2b67b8cb25342a0419ec71d183fb2ace6f9713
Johannes Johansson <143495185+mouwser@users.noreply.github.com>
2024-09-18 12:03:55 +0200

    Update App.vue

--- Grupp4forum.Dev.App/wwwroot/clientapp/src/App.vue
+++ Grupp4forum.Dev.App/wwwroot/clientapp/src/App.vue
@@ -4,9 +4,9 @@
         <img src="@/assets/flowertopright.png" alt="Right Image" class="corner-image right-image" />
         <nav>
             <router-link to="/" class="btn custom-primary">Hem</router-link>
-            <router-link to="/register" class="btn custom-primary">Registrera dig</router-link>
-            <router-link to="/login" class="btn custom-primary">Logga in</router-link>
             <router-link to="/forum" class="btn custom-primary">Forum</router-link>
+            <router-link to="/kategorier" class="btn custom-primary">Kategorier</router-link>
+            <router-link to="/login" class="btn custom-primary">Logga in</router-link>
         </nav>
         <router-view />
 
@@ -28,22 +28,20 @@
         padding: 30px;
     }
 
-
-
-
+   
+   
 
     /* Custom button styles */
     .btn.custom-primary {
-        background-color: #6ECB63;
+        background-color: #6ECB63; 
         color: black;
-        padding: 10px;
-        border-radius: 5px;
+        padding: 10px; 
+        border-radius: 5px; 
         text-decoration: none;
-        display: inline-block;
+        display: inline-block; 
         margin: 10px;
-        border-color: black;
+        border-color:black;
     }
-
         .btn.custom-primary:hover {
             background-color: #5CA44A;
         }
@@ -52,6 +50,7 @@
 
     .corner-image {
         position: absolute;
+       
         height: auto;
     }
 
@@ -64,4 +63,5 @@
         top: 0;
         right: 0;
     }
+
 </style>
